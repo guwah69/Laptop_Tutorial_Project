5fcc96c21f18be25f4cb07c96e173d273174b612
Goegre Uwah <guwah699@gmail.com>
2016-07-14 09:23:32 -0700

    Testing commit

--- Test3/src/postppp/YouTest.java
+++ Test3/src/postppp/YouTest.java
@@ -1,11 +1,19 @@
 package postppp;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class YouTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		System.out.println("Testing Test3");
+		
+		WebDriver driver = new FirefoxDriver ();
+		
+		driver.get("http://bbc.co.uk/");
+		
 
 	}
 
---
+++ src/main/java/FirstTest/FirstClassTest.java
@@ -0,0 +1,13 @@
+package FirstTest;
+
+public class FirstClassTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Say Hello World");
+		
+
+	}
+
+}
